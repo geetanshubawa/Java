c948b8974141825f6ab86dc82d0d2d0463b5fb7c
geetanshubawa <119449190+geetanshubawa@users.noreply.github.com>
2022-12-06 09:42:47 -0800

    Add files via upload

---
+++ condition/src/Main.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner s1 = new Scanner(System.in);
+
+        //double x = s1.nextDouble();
+        //double num2 = s1.nextDouble();
+
+       // if (x >=1) {
+           //System.out.println("y=" + 2*x+1);
+       // }
+        //else  {
+           // System.out.println(Math.pow(x, 2));
+
+         /*if( 1 <= x && x <= 10){
+                System.out.println("Given number is from Group 1");
+            } else if (11 <= x && x <= 20) {
+                System.out.println("Given number is from Group 2" );
+            } else if (21 <= x && x <= 30) {
+                System.out.println("Given number is from Group 3" );
+            } else if (31 <= x && x <=40){
+                System.out.println("Given number is from Group 4" );
+            } else if (41 <= x && x <= 50) {
+                System.out.println("Given number is from Group 5" );
+            } else  {
+                System.out.println(" OUT OF ORDER");
+            }/*
+
+            if(x == 1) {
+                System.out.println("Monday");
+            } else if(x == 2){
+                System.out.println("Tuesday");
+            }
+            else if(x == 3) {
+                System.out.println("Wednesday");
+            }
+            else if (x == 4) {
+                System.out.println("Thursady");
+            }
+                else if (x== 5) {
+                    System.out.println("Friday");
+                }
+               else if(x ==6) {
+                System.out.println("Saturday");
+            }
+               else if(x == 7){
+                System.out.println("Sunday");
+            }*/
+       //String user, password;
+       // System.out.println("Enter the username");
+       // user = s1.next();
+        //System.out.println("Enter the password");
+        //password = s1.next();
+        //if (user.equals("java") && password.equals ("Test1234")){
+           // if (user.equals("java") && password.equals("Test1234$")) {
+               // System.out.println("Both username and password are correct");
+            }// else if (!password.equals("Test1234$") && user.equals("java")) {
+              //  System.out.println("Password is wrong");
+            //} else if (!user.equals("java") && password.equals("Test1234$")) {
+           // } else {
+           //     System.out.println("Both are wrong");
+            }
+           // System.out.println("*********************");
+           // System.out.println("* Username = " + user+ "       *") ;
+           // System.out.println("* Password = " + password + "  *") ;
+            //System.out.println("*********************");
+
+
+
