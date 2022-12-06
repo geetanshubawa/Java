1060a901fff89e3c644105d1dbff6d42e6d2c29f
geetanshubawa <119449190+geetanshubawa@users.noreply.github.com>
2022-12-06 09:41:00 -0800

    Add files via upload

---
+++ assignment 4/src/Main.java
@@ -0,0 +1,85 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+                // 5th pattern
+                {
+            /*for(int i = 1;i<=5;i++)
+            {
+                for(int j = i;j<5;j++)
+                {
+                    System.out.print(" ");
+                }
+                for(int k = 1;k<=2*i-1;k++)
+                {
+                    System.out.print("*");
+                }
+                System.out.println("\n");*/
+
+
+                    // 9th pattern
+
+                /*for(int i = 1;i<=5;i++)
+                {
+                    for(int j = i;j<5;j++)
+                    {
+                        System.out.print(" ");
+                    }
+                    for(int k = 1;k<=i;k++)
+                    {
+                        System.out.print("*");
+                    }
+                    System.out.print("\n");
+                }
+                for(int i = 4;i>=1;i--)
+                {
+                    for(int j = i;j<5;j++)
+                    {
+                        System.out.print(" ");
+                    }
+                    for(int k = 1;k<=i;k++)
+                    {
+                        System.out.print("*");
+                    }
+                    System.out.print("\n");*/
+
+                    // 12th pattern
+
+                    {
+                        /*for (int i = 1; i <= 5; i++) {
+                            for (int j = i; j <= 5; j++) {
+                                System.out.print("*");
+                            }
+                            System.out.print("\n");
+                        }
+                        for (int i = 4; i >= 1; i--) {
+                            for (int j = i; j <= 5; j++) {
+                                System.out.print("*");
+                            }
+
+                            System.out.print("\n");*/
+                            // 7th pattern
+                            {
+                    for (int i = 1; i <= 5; i++) {
+                        for (int j = i; j < 5; j++) {
+                            System.out.print(" ");
+                        }
+                        for (int k = 1; k <= 2*i - 1; k++) {
+                            System.out.print("*");
+                        }
+                        System.out.print("\n");
+                    }
+                    for (int i = 4; i >= 1; i--) {
+                        for (int j = i; j < 5; j++) {
+                            System.out.print(" ");
+                        }
+                        for (int k = 1; k <= 2 * i - 1; k++) {
+                            System.out.print("*");
+                        }
+                        System.out.print("\n");
+                            }
+                        }
+                    }
+                }
+
+            }}
