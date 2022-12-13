da38504e4148b23e61481355676a9d437025af40
geetanshubawa <119449190+geetanshubawa@users.noreply.github.com>
2022-12-12 16:22:00 -0800

    Add files via upload

---
+++ untitle12d/src/Main.java
@@ -0,0 +1,82 @@
+ import java.util.Scanner;
+ import java.lang.Math;
+public class Main {
+    public static void main(String[] args
+    )
+    {
+  /* String name, fname, uname, pass;
+    name="geetanshu";
+    fname="bawa";
+    uname="bawageet";
+    pass="bawa1234";
+        System.out.println("first name: " + name );
+        System.out.println("last name: " + fname);
+        int num1, num2 , num3, sum, sub, mul;
+        double div;
+
+        sum = num1 + num2;
+        sub = num1 - num2;
+        mul = num1 * num2;
+        div = (double)num1/num2;
+        System.out.println("sum= " +sum);
+        System.out.println("subtraction = " +sub);
+        System.out.println("prouct= " +mul);
+        System.out.println("division= " +div);
+        double num1 = 10;
+        double num2 = 12;
+        double num3 = 14;
+        System.out.println("Average:" +(num1 + num2 + num3)/ 3);
+
+        System.out.println("Enter 3 numbers");
+        Scanner s1 = new Scanner(System.in);
+        int num1 = s1.nextInt();
+        int num2 = s1.nextInt();
+        Double num3 = s1.nextDouble();
+
+
+        double sum = num1+num2;
+        double sub = num1-num2;
+        System.out.println(" sum of two numbers " + sum );
+        System.out.println("Average:" +(num1 + num2 + num3)/ 3);
+        System.out.println("Enter the radius");
+
+        //double pi = Math.PI;
+        double radius = s1.nextDouble(); ;
+        double area = (Math.PI*(radius*radius));
+        double perimeter = (2*Math.PI*radius);
+
+        System.out.println("The radius of the circle = " + radius);
+        System.out.println("The area of the circle = " + area);
+        System.out.println("The perimeter of circle = " + perimeter);
+
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("Enter a ");
+        int a= s1.nextInt();
+        System.out.println("Enter b ");
+        int b = s1.nextInt();
+        System.out.println("Enter c ");
+        int c =  s1.nextInt();
+        System.out.println("Enter d ");
+        int d = s1.nextInt();
+        System.out.println("Enter x ");
+        double x = s1.nextDouble();
+
+        double result = ((a * (x*x))  + b) / ((c * x) - d) ;
+
+        System.out.println("The result of the equation is: " + result);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
