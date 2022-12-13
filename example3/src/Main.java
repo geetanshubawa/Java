420457e9bdbccdd463b45960a26d45788402bd0b
geetanshubawa <119449190+geetanshubawa@users.noreply.github.com>
2022-12-12 16:15:25 -0800

    Add files via upload

---
+++ example3/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+       /* System.out.println("Never put off till tomorrow");
+        System.out.println();
+        System.out.println("what you can do");
+        System.out.println(" the day ");
+        System.out.println();
+        System.out.println("after"+ " " + "tomorrow.");*/
+
+
+           /* int base = x;
+            int power = y;
+            int result = 1;
+                for (int i = 0; i < y; i++) {
+                    result *= y;
+
+                    System.out.println(x + " raise to " + y + " = " + result);
+
+
+        int x, n, sum;
+        double sum = i;
+        for(int i = 1, i <=n;i++);
+        {
+            sum = sum +(power(x,i)/ fact(i));
+        }
+        System.out.println(sum);*/
+
+
+
+
+                }
+
+
+
