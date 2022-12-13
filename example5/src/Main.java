ec1ad01c7dadaeea127d01da4b08611534fff23e
geetanshubawa <119449190+geetanshubawa@users.noreply.github.com>
2022-12-12 16:16:07 -0800

    Add files via upload

---
+++ example5/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("BBBB    EEEEE     A      TTTTT    RRRR    III   X   X");
+        System.out.println("B   B   E        A  A      T      R   R    I    X   X");
+        System.out.println("B   B   E       A    A     T      R   R    I     X X");
+        System.out.println("BBBB    EEE     AAAAAA     T      RRRR     I      X  ");
+        System.out.println("B   B   E       A    A     T      R   R    I     X X");
+        System.out.println("B   B   E       A    A     T      R   R    I    X    X");
+        System.out.println("BBBB    EEEEE   A    A     T      R   R    I    X    X");
+    }
+}
