50b79d038f75812009e424a391eadd50e09d73e3
geetanshubawa <119449190+geetanshubawa@users.noreply.github.com>
2022-12-12 16:11:59 -0800

    Add files via upload

---
+++ project 2/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+       /*  System.out.println("Never put off till tomorrow");
+        System.out.println();
+        System.out.print("what you can do");
+        System.out.println(" the day");
+        System.out.println();
+        System.out.println("after" + " " + "tomorrow.");
+
+                int i = 10;
+                int j = 12;
+
+                System.out.println("The value of i is " + i);
+                System.out.println("The value of j is " + j);*/
+
+        
+
+
+            }
+        }
+
