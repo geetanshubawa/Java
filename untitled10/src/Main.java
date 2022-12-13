0a0b263a4dcbcad8d463d9d8f7970360a1a97c17
geetanshubawa <119449190+geetanshubawa@users.noreply.github.com>
2022-12-12 16:23:05 -0800

    Add files via upload

---
+++ untitled10/src/Main.java
@@ -0,0 +1,118 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner s1 = new Scanner(System.in);
+
+
+       /*int arr[] = new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = s1.nextInt();
+        }
+        // System.out.println("The number of array:");
+
+       for (int i = 0; i < 10; i++) {
+
+            System.out.println(arr[i]);
+        }
+
+            for (int i = 0; i < 5; i++) {
+                arr[i] = (int)Math.pow(arr[i]*2);
+            }
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(arr[i]);
+                }
+
+        int arr2[] = new int[10];
+            for (int i = 0; i< 10; i++) {
+                arr[i] = s1.nextInt();
+
+            }
+        int arr3[] = new int[10];
+
+        for (int i = 0; i< 10; i++) {
+           arr3[i]=arr[i]+arr2[i];
+        }
+
+        for (int i = 0; i< 10; i++) {
+            System.out.println(arr3[i]);*/
+
+       /* int max = arr[0];
+        for(int i = 0; i<5 ; i++){
+            if (arr[i]> max){
+                max = arr[i];
+                System.out.println("enter the five numbers");
+
+            }*/
+
+
+            /*int min = arr[0];
+            for (int i = 0; i > 5; i ++){
+            if (arr[i]< min) {
+                min = arr[i];
+            }
+
+            }
+        System.out.println("Min ="+ min);*/
+
+        /*int num = s1.nextInt();
+        System.out.println("Array:" + num);
+        int[] arr = new int[num];
+        int[] arr2 = new int[num];
+        for (int i = 0; i < num; i++) {
+            arr[i] = s1.nextInt();
+        }
+        for (int i = 0; i < num; i++) {
+            int fact = 1;
+            for (int j = 1; j < arr[i]; j++) {
+                fact = fact * j;
+            }
+            arr2[i] = fact;
+        }
+
+        for (int i = 0; i < num; i++)
+        {
+        System.out.println(arr2[i]);
+        }
+        */
+        /* Scanner s1 = new Scanner(System.in);
+        System.out.println("enter min:");
+        int min = s1.nextInt();
+        System.out.println(" enter max:");
+        int max = s1.nextInt();
+
+
+        Random r1 = new Random();
+        int []arr = new int[5];
+        for (int i= 0; i<5 ; i++) {
+            arr[i] = r1.nextInt(max - min +1)+min;
+        }
+
+            for (int i = 0; i <5; i++){
+              System.out.println(arr[i]);
+        int [][] arr = new int[3][4];
+
+        for(int=0; i <3; i ++){
+
+        {for (int j= 0; j<4; j++)            {
+        }
+             arr[i][j] = s1.nextInt();
+            for (int i=0; i<3 ; i++){
+
+                for(int j= 0 ; j<4; j++){
+                    System.out.println(arr[i][j]);}*/
+        }
+}
+
+
+
+
+
+
+
+
+
+
