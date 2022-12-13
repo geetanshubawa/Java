7e7c309705fd113ae365bf0dc525f7f7c5450f94
geetanshubawa <119449190+geetanshubawa@users.noreply.github.com>
2022-12-12 16:19:56 -0800

    Add files via upload

---
+++ sum/src/Main.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+public class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello world!");
+            Scanner s1 = new Scanner(System.in);
+            System.out.println("Enter the first number:");
+            int num1 = s1.nextInt();
+            /*System.out.println("Enter the second number");
+            int num2 = s1.nextInt();
+           /* if (num1 > num2) {
+                int temp = num1;
+                num1 = num2;
+                num2 = temp;
+            }
+           // int m = 0;
+          //  for (int i = num1; i <= num2; i++) {
+                if (i % 2 == 0) {
+                  //  System.out.println(i);
+                 //   m = m + i;
+                }
+            }
+
+          //  System.out.println(m);
+
+              int fact=1;
+              for(int i=1;i<=num1;i++){
+        fact=fact*i;
+    }
+  System.out.println("Factorial of "+num1+" is: "+ fact);*/
+           /* Scanner sc = new Scanner(System.in);
+
+            System.out.println("Enter a base number: ");
+
+            double x=sc.nextDouble();
+
+            System.out.println("Enter a power number: ");
+
+            double n=sc.nextDouble();
+
+            double r=x;
+
+            for (int i=1;i<n;i++)
+            {
+                r=r*x;
+            }
+            System.out.println("exponent value of "+x+"^"+n+" is :"+r);*/
+           /* Scanner sc = new Scanner(System.in);
+
+
+            System.out.println("Please Enter any number to Find Factors: ");
+            int Number = sc.nextInt();
+
+            for (int i = 1; i <= Number; i++) {
+                if (Number % i == 0 && i % 2 == 0) {
+                    System.out.println(i);*/
+
+                }
+
+            }
+
+
+        
